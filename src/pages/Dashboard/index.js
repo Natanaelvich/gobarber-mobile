a34227a5a855bef2843acade9d3bf9d8942a7277
Natanael Lima <taelima1997@gmail.com>
2020-02-16 15:27:23 -0300

    styled  appointent list dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Container } from './styles';
 import BackGround from '~/components/Background';
+import Appointment from '~/components/Appoiment';
 
+import { Container, Title, List } from './styles';
+
+const data = [1, 2, 3, 4, 5, 6];
 export default function Dashboard() {
-  return <BackGround />;
+  return (
+    <BackGround>
+      <Container>
+        <Title>Agendamentos </Title>
+
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({ item }) => <Appointment data={item} />}
+        />
+      </Container>
+    </BackGround>
+  );
 }
 
 Dashboard.navigationOptions = {
