c316113a516702bf012931b2f4042a031f795d10
Natanael Lima <taelima1997@gmail.com>
2020-02-16 17:11:23 -0300

    create function delete appointment and create styles

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '~/services/api';
+
 import BackGround from '~/components/Background';
 import Appointment from '~/components/Appoiment';
 
@@ -8,15 +9,38 @@ import { Container, Title, List } from './styles';
 
 const data = [1, 2, 3, 4, 5, 6];
 export default function Dashboard() {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
+
+      setAppointments(response.data);
+    }
+
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+
+    setAppointments(
+      appointments.map(a =>
+        a.id === id ? { ...a, canceled_at: response.data.canceled_at } : a
+      )
+    );
+  }
   return (
     <BackGround>
       <Container>
         <Title>Agendamentos </Title>
 
         <List
-          data={data}
-          keyExtractor={item => String(item)}
-          renderItem={({ item }) => <Appointment data={item} />}
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointment onCancel={() => handleCancel(item.id)} data={item} />
+          )}
         />
       </Container>
     </BackGround>
